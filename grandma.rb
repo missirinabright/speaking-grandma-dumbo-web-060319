e23c75a66a48c62fd2ab2a171defdbd335f035fe
Irina Bright <missirinabright@gmail.com>
2019-05-15 18:34:36 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,5 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase = "I LOVE YOU GRANDMA!"
+  if phrase == "I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+    
     

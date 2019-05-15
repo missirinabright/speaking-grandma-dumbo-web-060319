2ebf3362dae988afc5c4f918d867705c3ed1de92
Irina Bright <missirinabright@gmail.com>
2019-05-15 18:37:49 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,7 +16,8 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
     
-  elsif phrase == 
+  elsif phrase == "Hi"
+  puts ""
   
 else puts "NO, NOT SINCE 1938!"
 end
